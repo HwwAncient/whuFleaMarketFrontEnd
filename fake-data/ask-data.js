c8970b1708a457c058c7b7ba7875955757cbc0bf
HwwAncient <wanweihe@whu.edu.cn>
2019-04-26 01:19:56 +0800

    add new fake data deal_Database

--- fake-data/ask-data.js
+++ fake-data/ask-data.js
@@ -14,7 +14,7 @@ var ask_Database = [{
     mode: 0
   },
   {
-    id: 1,
+    id: "2",
     owner: {
       id: "1",
       name: "李四",
@@ -29,7 +29,7 @@ var ask_Database = [{
     mode: 0
   },
   {
-    id: 1,
+    id: "3",
     owner: {
       id: "1",
       name: "王五",
@@ -45,6 +45,54 @@ var ask_Database = [{
   }
 ]
 
+const deal_Database = [{
+  id: "4",
+  owner: {
+    id: "1",
+    name: "张三",
+    headSrc: "/images/home/user-image1.jpg",
+    school: "计算机学院"
+  },
+  address: "C7舍",
+  time: "8:40",
+  content: "求乒乓球拍 全新",
+  status: 0,
+  category: 1,
+  mode: 0
+},
+{
+  id: "5",
+  owner: {
+    id: "1",
+    name: "李四",
+    headSrc: "/images/home/user-image2.jpg",
+    school: "国家网络安全学院"
+  },
+  address: "C4舍",
+  time: "9:50",
+  content: "收托福词汇书 新旧均可",
+  status: 0,
+  category: 1,
+  mode: 0
+},
+{
+  id: "6",
+  owner: {
+    id: "1",
+    name: "王五",
+    headSrc: "/images/home/user-image3.jpg",
+    school: "测绘学院学院"
+  },
+  address: "13舍",
+  time: "11:50",
+  content: "收安卓机 价格好商量",
+  status: 0,
+  category: 1,
+  mode: 0
+}
+]
+
 module.exports = {
-  askList: ask_Database
+  askList: ask_Database,
+  dealList: deal_Database
 }
