12df5cf3749afb88444fa3d6466032cc29a0248f
HwwAncient <wanweihe@whu.edu.cn>
2019-04-27 02:02:22 +0800

    update the ask-card component

--- components/home/ask-card/index.js
+++ components/home/ask-card/index.js
@@ -1,22 +1,28 @@
 Component({
 
   properties: {
-    cardItem:Object
-
+    cardItem: Object
   },
 
   data: {
-
+    university: "武汉大学",
+    proxy: "代理点",
+    free: "自由发布"
   },
 
   methods: {
 
-    onButtonTap:function(){
+    onButtonTap: function() {
       var dealId = this.properties.cardItem.id
-      console.log(dealId)
-      // TODO 利用dealId跳转到商品详情页面
+      //利用dealId跳转到商品详情页面
+      const detailProxy = "/pages/home/deal/detail-proxy/detail-proxy"
+      const detailFree = "/pages/home/deal/detail-free/detail-free"
+      wx.navigateTo({
+        url: (this.properties.cardItem.mode == 1 ? detailProxy : detailFree) + "?dealId=" + dealId
+      })
+
     }
 
   }
-  
+
 })
