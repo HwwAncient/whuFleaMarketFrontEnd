7f28f8b8ad3ed359b0f6afc0b1abbc73b9e91e85
HwwAncient <wanweihe@whu.edu.cn>
2019-04-30 02:07:30 +0800

    add the map and bottom-bar

--- pages/home/deal/detail-free/detail-free.js
+++ pages/home/deal/detail-free/detail-free.js
@@ -3,23 +3,67 @@ var data_base = require('../../../../fake-data/ask-data.js')
 Page({
 
   data: {
-    dealDetailFree:null
+    dealDetailFree: null,
+    collectImgUrl: "/images/home/deal/detail/collect.png",
+    chatImgUrl: "/images/home/deal/detail/chat.png",
+    longitude: 0,
+    latitude: 0,
+    markers: [],
+    // polyline: [{
+    //   points: [{
+    //     latitude: 30.53925933944048,
+    //     longitude: 114.35536980628967,
+    //   }, {
+    //     latitude: 30.54365933944048,
+    //     longitude: 114.35536980628967,
+    //   }],
+    //   color: "#FF0000DD",
+    //   width: 4,
+    //   dottedLine: false
+    // }]
   },
 
   onLoad: function(options) {
     let dealId = options.dealId
     console.log(dealId)
-    // TODO 根据dealId向服务器请求详细买卖数据，此处用假数据代替
+
+    // map中标记目的位置的marker
+    var marker = {
+      // iconPath:"/images/1.png",
+      id: 0,
+      latitude: 0,
+      longitude: 0,
+      width: 25,
+      height: 25
+    }
+
+    // TODO 根据dealId向服务器请求详细买卖数据，此处用假数据dealDetailFree代替
+    let latitude = data_base.dealDetailFree.address.latitude;
+    let longitude = data_base.dealDetailFree.address.longitude;
+    marker.latitude = latitude;
+    marker.longitude = longitude;
+    var markers = [marker]
     this.setData({
-      dealDetailFree: data_base.dealDetailFree
+      dealDetailFree: data_base.dealDetailFree,
+      longitude: longitude,
+      latitude: latitude,
+      markers:markers
     })
 
   },
 
   // 动态设置页面标题
-  onReady: function () {
+  onReady: function() {
     wx.setNavigationBarTitle({
       title: this.data.dealDetailFree.title
+    })
+  },
+
+  // 点击小程序地图组件上的目的地marker跳转到微信原生地图
+  markertap(e) {
+    wx.openLocation({
+      latitude: this.data.latitude,
+      longitude: this.data.longitude,
     })
   }
 
