57989760eb1dd94cf1258c336cd0c8fa00bdc295
HwwAncient <wanweihe@whu.edu.cn>
2019-04-30 02:06:45 +0800

    copy the content of detail-free at the moment

--- pages/home/deal/detail-proxy/detail-proxy.js
+++ pages/home/deal/detail-proxy/detail-proxy.js
@@ -6,7 +6,7 @@ Page({
     dealDetailProxy: null
   },
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     let dealId = options.dealId
     console.log(dealId)
     // TODO 根据dealId向服务器请求详细买卖数据，此处用假数据代替
@@ -14,6 +14,13 @@ Page({
       dealDetailProxy: data_base.dealDetailProxy
     })
 
+  },
+
+  // 动态设置页面标题
+  onReady: function() {
+    wx.setNavigationBarTitle({
+      title: this.data.dealDetailProxy.title
+    })
   }
 
 })
